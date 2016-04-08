060f20ad193febd1a2badd7a4152dbda54e3abcf
Sam Wak <samuelwaknine@gmail.com>
2016-04-09 00:34:50 +0300

    Added User Post association

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ActiveRecord::Base
 
 	#Add constraint max 140
 	validates :content, :length => { :maximum => 140 }
+
+	#Add User Post association
+	belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+
+	#Add User Post association
+	has_many :posts
 end
